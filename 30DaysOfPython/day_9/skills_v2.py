2aa49293cd6d523efb17382bbe810b5faa27d991
s4126823 <s4126823@student.rmit.edu.au>
2026-03-29 14:25:24 +1100

    added, string conditional that converts list of skills to string depending on amount of items in skill list. Mainly for formatting and gramatical correctness.

--- 30DaysOfPython/day_9/skills_v2.py
+++ 30DaysOfPython/day_9/skills_v2.py
@@ -1,6 +1,5 @@
-
 # DATA
-person_dict = {'skills': ['JavaScript', 'Node', 'React', 'MongoDB', 'Python'],}
+person_dict = {'skills': ['JavaScript', 'Node', 'MongoDB', 'Python'],}
 
 skill_categories = {"fullstack": ["React", "Node", "MongoDB"],
                     "backend": ["Node", "Python", "MongoDB"],
@@ -25,32 +24,40 @@ title = "unknown"
 for category, required_skills in skill_categories.items(): # Loops through keys in dict, assigned category to keys and required_skill to values                                                        
     for required_skill in required_skills:
         if required_skill in skill_list:
-            skill_scores[category] += 1
-
-
+            skill_scores[category] += 1   # If a required skill is found in the person's skill list, the category the skill is associated with is assigned one point
 
 # Determining highest score
-scores = skill_scores.values()
-highest_score = max(scores)
+scores = skill_scores.values()  
+highest_score = max(scores)    # Evalutes highest score from a list of the categories' scores
 
 high_score_categories =[]
 
-for score in skill_scores.items():
-    if score[1] == highest_score:
+for score in skill_scores.items():       # Checks the scores, compares those scores with the highest score found and if a score is equal to the highest, 
+    if score[1] == highest_score:        # its corresponding category is noted in a list
         high_score_categories.append(score[0])
 
-if len(high_score_categories) == 1:
-    title = high_score_categories[0]
+
+# Determining title from highest scoring category
+if len(high_score_categories) == 1:    # Checks if only one category has the highest score.
+    title = high_score_categories[0]   
+# Determining title from priorities if there is a tie in score
+elif len(high_score_categories) > 1:   # Checks if more than one category has the highest score
+    current_best = high_score_categories[0]
+    for category in high_score_categories:
+        if skill_priorities[current_best] > skill_priorities[category]:
+            current_best = category
+    title = current_best
 
 
-# Determining title from priorities
-current_best = high_score_categories[0]
-for category in high_score_categories:
-    if skill_priorities[current_best] > skill_priorities[category]:
-        current_best = category
 
-title = current_best
+# Outputting Job title depending on skillset
+if len(skill_list) == 1:
+    skills = skill_list[0]
+elif len(skill_list) == 2:
+    skills = "and ".join(skill_list)
+else:
+    start_of_list = ", ".join(skill_list[:-1]) 
+    end_of_list = ", and " + skill_list[-1]
+    skills = start_of_list + end_of_list
 
-
-
-print(f"Person is a {title} developer!")
+print(f"Person is proficient in {skills}. Therefore is a {title} developer!")
